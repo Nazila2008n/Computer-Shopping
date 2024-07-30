04b5d31554fa08aa04ac1e34fb6818d061b6ba4f
Nazila2008n <s.babayeva83@gmail.com>
2024-07-30 11:47:11 +0400

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Shop from './pages/Shop'
+import Login from './pages/Login'
+import Registration from './pages/Registration'
+import Profile from './pages/Profile'
+import ErrorPage from './pages/ErrorPage'
 
 function App() {
  
 
   return (
     <>
-      app
+      <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path="/shop"  element={<Shop/>}/>
+        <Route path="/log-in"  element={<Login/>}/>
+        <Route path="/registration"  element={<Registration/>}/>
+        <Route path="/profile"  element={<Profile/>}/>
+        <Route path="/*"  element={<ErrorPage/>}/>
+      </Routes>
     </>
   )
 }
